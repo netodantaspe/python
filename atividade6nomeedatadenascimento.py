50a08a873902a392f5ec83c8fd23c586d16dd1c5
netodantaspe <110497895+netodantaspe@users.noreply.github.com>
2022-08-05 18:02:07 -0300

    Add files via upload

---
+++ atividade6nomeedatadenascimento.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome completo ")
+anonasc = int(input("digite o ano que nasceu "))
+conta = 2022-anonasc
+
+##eu to ligado que tenho que fazer um laço de repetição para ano de nascimento ser acima de... e abaixo de....
+
+
+print("seu nome é ",nome, "e este ano você completará ",conta, "anos, parabéns!!!")
